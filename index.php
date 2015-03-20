f2aa0e1889f245169ee23ee2c67b1c165ee77f33
Piotr <starzec.piotr.12@gmail.com>
2015-03-20 12:26:16 +0000

    Added Subcategory and Project classes, small changes done to old classes. Test classes were generared - need to be done

--- index.php
+++ index.php
@@ -270,7 +270,9 @@ include './php/scripts.php'; // gets scripts
 		<!--</section>-->
 	</body>
 	<!--standard scripts start-->
-	<script src="js/vendor/jquery-1.11.2.js"></script>
+	
+	<!--<script src="js/vendor/jquery-1.11.2.js"></script>-->
+	<script src="//code.jquery.com/jquery-1.11.2.js"></script>
 
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
 
